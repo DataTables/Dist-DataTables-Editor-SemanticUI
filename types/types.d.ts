a5772390b1d9b4df2f87960d1037578eccbf3bca
Allan Jardine <allan@datatables.net>
2026-09-18 15:41:38 +0000

    Dist: Fix types for the host Editor

--- types/types.d.ts
+++
@@ -1,7 +0,0 @@
-
-// Dist-DataTables-Editor-SemanticUI integration with SemanticUI exports the main Editor class having
-// set default values to complete the ingeration.
-import Editor from "datatables.net-editor";
-
-export default Editor;
-
